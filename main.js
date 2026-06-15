86ddf8d02dfd011160e8b7efb11470fe6b84bbb4
DreamsOfGrandeur <94984208+DreamsOfGrandeur@users.noreply.github.com>
2026-06-15 15:52:20 -0400

    Added file upload functionality

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const fileInput = document.querySelector("#fileInput");
-fileInput.addEventListener('changed', onNewFileUploaded);
+fileInput.addEventListener('change', () => { onNewFileUploaded(fileInput.files); });
 
 const page = document.querySelector("#page");
 page.addEventListener('dragenter', onPageDragEnter);
@@ -7,25 +7,46 @@ page.addEventListener('dragover', onPageDragOver);
 page.addEventListener('drop', onPageDrop);
 
 
-function onPageDragEnter() {
-    e.stopPropagation();
-    e.preventDefault();
+function onPageDragEnter(event) {
+    event.stopPropagation();
+    event.preventDefault();
 }
 
 
-function onPageDragOver() {
-    e.stopPropagation();
-    e.preventDefault();
+function onPageDragOver(event) {
+    event.stopPropagation();
+    event.preventDefault();
 }
 
 
-function onPageDrop() {
-    e.stopPropagation();
-    e.preventDefault();
+function onPageDrop(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    console.log(event.dataTransfer.files[0]);
+
+    onNewFileUploaded(event.dataTransfer.files);
 }
 
 
 function onNewFileUploaded(files) {
-    const file = this.files[0];
+    const file = files[0];
 
+    if (!file) {
+        console.log("ERROR: no file uploaded!");
+        return;
+    }
+
+    if (!file.type.startsWith("text")) {
+        console.log("ERROR: file type unsupported (please stick to text files!)");
+    }
+
+    const docNameLabel = document.querySelector("#docNameLabel");
+    docNameLabel.textContent = file.name;
+
+    const fReader = new FileReader();
+    fReader.onload = () => {
+        page.value = fReader.result;
+    }
+    fReader.readAsText(file);
 }
